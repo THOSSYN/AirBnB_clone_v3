cff63d0a36c546d4ee4c53679235b1c2086646a1
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-30 20:28:37 +0000

    pycodestyle

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,6 +7,7 @@ from models.state import State
 from models.city import City
 from models import storage
 
+
 @app_views.route('/states/<state_id>/cities', methods=['GET'])
 def get_cities_by_state_id(state_id):
     """Retrieves the cities of that state_id"""
@@ -19,6 +20,7 @@ def get_cities_by_state_id(state_id):
         city_dict = [city.to_dict() for city in state.cities]
         return jsonify(city_dict)
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'])
 def get_cities_by_city_id(city_id):
     """Retrieves a city object by its city_id"""
@@ -29,6 +31,7 @@ def get_cities_by_city_id(city_id):
             abort(404)
 
         return jsonify(city.to_dict())
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'])
 def delete_cities_by_id(city_id):
@@ -41,8 +44,9 @@ def delete_cities_by_id(city_id):
 
         storage.delete(city)
         storage.save()
-    
+
         return jsonify({}), 200
+
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
 def create_cities_by_id(state_id):
@@ -66,6 +70,7 @@ def create_cities_by_id(state_id):
         storage.new(new_city)
         storage.save()
         return jsonify(new_city.to_dict()), 201
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'])
 def update_cities_by_id(city_id):
