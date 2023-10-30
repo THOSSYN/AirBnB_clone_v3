ce62ad269790daad1b5ac9ea26d01fcfc14c7039
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-30 20:18:19 +0000

    pycodestyle

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -6,13 +6,17 @@ from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
 
-@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+r = '/amenities'
+
+
+@app_views.route(r, methods=['GET'], strict_slashes=False)
 def get_amenities():
     """Renders all amenities view"""
     amenities = storage.all(Amenity).values()
     return jsonify([amenity.to_dict() for amenity in amenities])
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+
+@app_views.route(r + '/<amenity_id>', methods=['GET'], strict_slashes=False)
 def get_amenity_by_id(amenity_id):
     """Displays an endpoint for one amenity by id"""
     per_amenity = storage.get(Amenity, amenity_id)
@@ -23,18 +27,20 @@ def get_amenity_by_id(amenity_id):
     if per_amenity.id == amenity_id:
         return jsonify(per_amenity.to_dict())
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route(r + '/<amenity_id>', methods=['DELETE'], strict_slashes=False)
 def delete_amenity_by_id(amenity_id):
     """Deletes an amenity by its id"""
     per_amenity = storage.get(Amenity, amenity_id)
-    
+
     if per_amenity is None:
         abort(404)
-    
+
     storage.delete(per_amenity)
     storage.save()
 
     return jsonify({}), 200
+
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def post_amenity():
@@ -52,7 +58,8 @@ def post_amenity():
     storage.save()
     return jsonify(new_amenity.to_dict()), 201
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+
+@app_views.route(r + '/<amenity_id>', methods=['PUT'], strict_slashes=False)
 def put_amenity(amenity_id):
     """Updates an amenity instance"""
     per_amenity = storage.get(Amenity, amenity_id)
